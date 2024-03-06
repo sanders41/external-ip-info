61c9f8f53b0cf558dcd377478ca032da06b2403b
Paul Sanders <psanders1@gmail.com>
2024-03-06 11:00:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# External IP Information
+
+Prints the current external ip address and location.
+
+## Installation
+
+First clone the repository and change to the `external-ip-info` directory
+
+```sh
+git clone git clone git@github.com:sanders41/external-ip-info
+```
+
+```sh
+cargo install --path .
+```
+
+## How to use
+
+Run the program
+
+```sh
+external-ip-info
+```
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use anyhow::Result;
+use colored::*;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct IpInfo {
+    ip_addr: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct LocationInfo {
+    country: String,
+    region: String,
+    city: String,
+}
+
+fn get_ip_info() -> Result<IpInfo> {
+    let ip = reqwest::blocking::get("https://ifconfig.me/all.json")?.json::<IpInfo>()?;
+
+    Ok(ip)
+}
+
+fn get_location_info(ip: &str) -> Result<LocationInfo> {
+    let location =
+        reqwest::blocking::get(format!("http://ip-api.com/json/{ip}"))?.json::<LocationInfo>()?;
+
+    Ok(location)
+}
+
+fn main() {
+    let ip_info = if let Ok(ip) = get_ip_info() {
+        ip
+    } else {
+        eprintln!("{}", "Error retrieving ip information".red());
+        std::process::exit(1);
+    };
+    let location_info = if let Ok(location) = get_location_info(&ip_info.ip_addr) {
+        location
+    } else {
+        eprintln!("{}", "Error retrieving location information".red());
+        std::process::exit(1);
+    };
+
+    let ip_addr = format!("{}", ip_info.ip_addr.blue());
+    let country = format!("{}", location_info.country.blue());
+    let region = format!("{}", location_info.region.blue());
+    let city = format!("{}", location_info.city.blue());
+
+    println!("");
+    println!("ip address: {}", ip_addr);
+    println!("country: {}", country);
+    println!("region: {}", region);
+    println!("city: {}", city);
+}
